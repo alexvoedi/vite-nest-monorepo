b5f37181123aaf4606ab99f528baf3fbfd17a03c
Alexander Vödisch <voedisch.alexander@gmail.com>
2023-07-12 16:47:18 +0200

    init

--- apps/api/src/repl.ts
+++ apps/api/src/repl.ts
@@ -2,13 +2,7 @@ import { repl } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const replServer = await repl(AppModule);
-
-  replServer.setupHistory('.nestjs_repl_history', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
+  await repl(AppModule);
 }
 
 bootstrap();
